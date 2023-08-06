f60917a9876fdf3a04b3e3fb04a25de0915467fc
FabianaLino <fabianalino@icloud.com>
2023-08-06 19:04:44 -0300

    Atualização de html, css e lib´s react

--- promo-tenis/src/App.js
+++ promo-tenis/src/App.js
@@ -1,28 +1,67 @@
+import { motion } from "framer-motion";
+import { useEffect, useState, useRef } from "react";
 import "./App.css";
 
 function App() {
+    const [data, setData] = useState([]);
+    const carousel = useRef(null);
+
+    useEffect(() => {
+        fetch("http://localhost:3000/static/materialtenis.json")
+            .then((Response) => Response.json())
+            .then(setData);
+    }, []);
+
+    if (!data || !data.length) return null;
+
+    const handleLeftClick = (e) => {
+        e.preventDefault();
+        carousel.current.scrollLeft -= carousel.current.offsetWidth;
+    };
+
+    const handleRightClick = (e) => {
+        e.preventDefault();
+        carousel.current.scrollLeft += carousel.current.offsetWidth;
+    };
+
     return (
         <div className="container">
-            <div className="logo">
+            <motion.div animate={{ x: -30, y: 25 }} className="logo">
                 <img
-                    src="/static/images/logo-tenis2.png"
-                    width={350}
-                    height={200}
+                    src="/static/images/logolojatenis.png"
+                    width={270}
+                    height={210}
                     alt="Logo loja tenis"
                 />
+            </motion.div>
+            <div className="carousel" ref={carousel}>
+                {data.map((item) => {
+                    const { id, name, price, oldPrice, image } = item;
+                    return (
+                        <div className="item" key={id}>
+                            <img src={image} alt={name} />
+                            <div className="info">
+                                <span className="name">{name}</span>
+                                <span className="oldPrice">{oldPrice}</span>
+                                <span className="price">{price}</span>
+                            </div>
+                        </div>
+                    );
+                })}
             </div>
-            <div className="carousel">
-                <div className="item">
+            <div className="buttons">
+                <button onClick={handleLeftClick}>
                     <img
-                        src="https://imgcentauro-a.akamaihd.net/230x230/94313731.jpg"
-                        alt="Produto Tenis"
-                    />
-                    <div className="info">
-                        <span className="name">Promo Tenis</span>
-                        <span className="oldPrice">R$289,90</span>
-                        <span className="price">R$199,90</span>
-                    </div>
-                </div>
+                        src="/static/images/right_chevron_icon.png"
+                        alt="Scroll Left"
+                    ></img>
+                </button>
+                <button onClick={handleRightClick}>
+                    <img
+                        src="/static/images/right_chevron_icon.png"
+                        alt="Scroll Right"
+                    ></img>
+                </button>
             </div>
         </div>
     );
